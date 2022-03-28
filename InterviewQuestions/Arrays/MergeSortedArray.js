385aa155667e02f99040738839992aa88bbc017d
marynghiem <marynghiem46@gmail.com>
2022-03-28 12:24:11 -0700

    merge sort two sorted arrays, using pointers

--- InterviewQuestions/Arrays/MergeSortedArray.js
+++ InterviewQuestions/Arrays/MergeSortedArray.js
@@ -1,7 +1,7 @@
 //https://leetcode.com/problems/merge-sorted-array/
 
 //1st try (inefficent)
-
+//runtime: 104 ms
 //time and space complexity:
 //time: (n)log(n) for nums.sort
 //time: (n+m)log(n+m)
@@ -13,4 +13,29 @@ var merge = function (nums1, m, nums2, n) {
     indexNums2++;
   }
   nums1.sort((a, b) => a - b);
+};
+
+//second try
+//coded using pointers
+//faster runntime 67ms
+
+var merge = function (nums1, m, nums2, n) {
+  let first = m - 1;
+  let second = n - 1;
+  let i = m + n - 1;
+
+  while (second >= 0) {
+    let firstVal = nums1[first];
+    let secondVal = nums2[second];
+    if (firstVal < secondVal) {
+      nums1[i] = secondVal;
+      i--;
+      second--;
+    } else {
+      nums1[i] = firstVal;
+      nums1[i] = firstVal;
+      i--;
+      first--;
+    }
+  }
 };
