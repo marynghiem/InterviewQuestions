a9a07a6aa16864047150137b1cb6fad9487c6b71
marynghiem <marynghiem46@gmail.com>
2022-04-05 12:48:54 -0700

    removing duplicates from linked list

---
+++ InterviewQuestions/LinkedList/RemoveDuplicatesFromSortedList.js
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+//solution on the internet, need to try again on my own later
+var deleteDuplicates = function (head) {
+  if (head == null || head.next == null) return head;
+  let prev = head;
+  let p = head.next;
+  while (p != null) {
+    if (p.val == prev.val) {
+      prev.next = p.next;
+      p = p.next;
+      //no change prev
+    } else {
+      prev = p;
+      p = p.next;
+    }
+  }
+  return head;
+};
