b110b7e9b0ee968a4c7682a7943384a5c9cc637c
marynghiem <marynghiem46@gmail.com>
2022-04-06 14:22:45 -0700

    faster solution for palindrome linked list

--- InterviewQuestions/LinkedList/PalindromeLinkedList.js
+++ InterviewQuestions/LinkedList/PalindromeLinkedList.js
@@ -1,5 +1,6 @@
 //https://leetcode.com/problems/palindrome-linked-list/submissions/
 
+//first solution
 var isPalindrome = function (head) {
   let answer = [];
   while (head) {
@@ -17,4 +18,41 @@ var isPalindrome = function (head) {
     rightPointer--;
   }
   return true;
+};
+
+//second solution
+//get to the middle, reverse the second half and then compare
+var isPalindrome = function (head) {
+  if (!head || !head.next) {
+    return true;
+  }
+  let slow = head;
+  let fast = head;
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  slow.next = reverseList(slow.next);
+  slow = slow.next;
+  while (slow) {
+    if (slow.val !== head.val) {
+      return false;
+    }
+    slow = slow.next;
+    head = head.next;
+  }
+  return true;
+};
+
+var reverseList = function (head) {
+  let current = head;
+  let previous = null;
+  let next;
+  while (current !== null) {
+    next = current.next; // save the next number && increment next up.
+    current.next = previous; //change the next pointer to be reversed
+    previous = current; // increment the previous up
+    current = next; //increment the current up
+  }
+  return previous; //returning all previous values
 };
