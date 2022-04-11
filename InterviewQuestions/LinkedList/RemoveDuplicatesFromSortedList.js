4dc0b746a308e403f740d1c992279686e4b028be
marynghiem <marynghiem46@gmail.com>
2022-04-11 15:07:22 -0700

    added another solution for deleting duplicates in a linked list

--- InterviewQuestions/LinkedList/RemoveDuplicatesFromSortedList.js
+++ InterviewQuestions/LinkedList/RemoveDuplicatesFromSortedList.js
@@ -17,3 +17,16 @@ var deleteDuplicates = function (head) {
   }
   return head;
 };
+
+//easier solution to understand
+var deleteDuplicates = function (head) {
+  let current = head;
+  while (current !== null && current.next !== null) {
+    if (current.val === current.next.val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  return head;
+};
