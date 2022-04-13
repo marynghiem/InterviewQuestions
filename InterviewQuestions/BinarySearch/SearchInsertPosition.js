d61e577072f4fe006080edeae7661ff0d13d1af4
marynghiem <marynghiem46@gmail.com>
2022-04-13 15:46:14 -0700

    updated solution to correct answer

--- InterviewQuestions/BinarySearch/SearchInsertPosition.js
+++ InterviewQuestions/BinarySearch/SearchInsertPosition.js
@@ -1,9 +1,10 @@
 // Search Insert Position
 
+//need to try again later
 var searchInsert = function (nums, target) {
   let newArr = [...nums];
   let left = 0;
-  let right = newArr.length - 1;
+  let right = newArr.length;
   while (left < right) {
     let middle = Math.floor((left + right) / 2);
     if (newArr[middle] < target) {
