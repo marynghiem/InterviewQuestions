93cb3b6c6a0b60cb63200f671df5ae3db0e42177
marynghiem <marynghiem46@gmail.com>
2022-04-25 23:57:04 -0700

    completed squares of sorted array

---
+++ InterviewQuestions/Arrays/SquaresOfSortedArray.js
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/squares-of-a-sorted-array/
+
+var sortedSquares = function (nums) {
+  let result = [];
+  let left = 0;
+  let right = nums.length - 1;
+  let position = right;
+
+  while (left <= right) {
+    if (nums[left] ** 2 > nums[right] ** 2) {
+      result[position] = nums[left] ** 2;
+      position--;
+      left++;
+    } else {
+      result[position] = nums[right] ** 2;
+      position--;
+      right--;
+    }
+  }
+  console.log(result);
+  return result;
+};
--- InterviewQuestions/Strings/SubtractProductAndSum.js
+++ InterviewQuestions/Strings/SubtractProductAndSum.js
@@ -1,5 +1,6 @@
 //https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
 
+//my solution
 var subtractProductAndSum = function (n) {
   let numAsStrings = n.toString();
   let product = 1;
@@ -12,4 +13,17 @@ var subtractProductAndSum = function (n) {
   }
   let result = product - sum;
   return result;
+};
+
+//better solution, try again later
+var subtractProductAndSum = function (n) {
+  let product = 1,
+    sum = 0;
+  while (n > 0) {
+    const digit = n % 10; //if given 123, digit would be 3
+    n = parseInt(n / 10); // if given 123, n/10 would give 12.3 but parse int would turn it into an integer
+    product = product * digit;
+    sum += digit;
+  }
+  return product - sum;
 };
