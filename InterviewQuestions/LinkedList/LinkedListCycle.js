7bf77282d5fc89a4701dbd19d99b94c652c7d084
marynghiem <marynghiem46@gmail.com>
2022-04-28 14:09:25 -0700

    completed linked list cycle in different ways

---
+++ InterviewQuestions/LinkedList/LinkedListCycle.js
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/linked-list-cycle/
+
+//tried on my own, without looking at the solution but jo says i wouldve failed the interview bc it changes the input
+//use a hashmap if you dont want to change input
+var hasCycle = function (head) {
+  while (head) {
+    if (head.val == "checked") {
+      return true;
+    }
+    head.val = "checked";
+    head = head.next;
+  }
+  return false;
+};
+
+//correct solution with slow and fast method
+var hasCycle = function (head) {
+  if (head == null) {
+    return false;
+  }
+  let slow = head;
+  let fast = head.next;
+  while (slow != fast) {
+    if (fast == null || fast.next == null) {
+      return false;
+    }
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return true;
+};
