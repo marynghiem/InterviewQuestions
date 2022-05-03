aedab3d650186e9d72b7de087e8da46c64b50526
marynghiem <marynghiem46@gmail.com>
2022-05-03 11:56:27 -0700

    completed isSameTree using recursion

---
+++ InterviewQuestions/Trees/IsSameTree.js
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/same-tree/submissions/
+
+var isSameTree = function (p, q) {
+  if (p === null) {
+    return q === null;
+  }
+  if (q === null || p.val !== q.val) {
+    return false;
+  }
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
