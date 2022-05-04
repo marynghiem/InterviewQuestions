3a8d685c2e11b99d98fddeb4254ca21a1442f11c
marynghiem <marynghiem46@gmail.com>
2022-05-04 15:05:42 -0700

    invert binary tree

---
+++ InterviewQuestions/Trees/InvertBinaryTree.js
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/invert-binary-tree/
+
+var invertTree = function (root) {
+  const reverseNodes = (node) => {
+    if (node === null) {
+      return;
+    }
+    reverseNodes(node.left);
+    reverseNodes(node.right);
+
+    let temp = node.left;
+    node.left = node.right;
+    node.right = temp;
+  };
+  reverseNodes(root);
+  return root;
+};
