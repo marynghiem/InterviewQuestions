bfbd724e811d1ad530df7c1e824565e287471591
marynghiem <marynghiem46@gmail.com>
2022-05-17 11:50:33 -0700

    attempted two numbers again and documented my mistakes

--- MediumInterviewQuestions/LinkedList/AddTwoNumbers.js
+++ MediumInterviewQuestions/LinkedList/AddTwoNumbers.js
@@ -23,3 +23,24 @@ var addTwoNumbers = function (l1, l2) {
   }
   return result.next;
 };
+
+var addTwoNumbers = function (l1, l2) {
+  let sum = 0;
+  let current = new ListNode(0);
+  let result = current;
+  while (l1 || l2) {
+    if (l1) {
+      sum += l1.val;
+      l1 = l1.next;
+    }
+    if (l2) {
+      sum += l2.val;
+      l2 = l2.next;
+    }
+    current.next = new ListNode(sum % 10);
+    current = current.next;
+    sum > 9 ? (sum = 1) : (sum = 0);
+  }
+
+  return result.next;
+};
