3239370065b982f4deedf5e8e7a589075174926b
marynghiem <marynghiem46@gmail.com>
2022-05-19 13:42:18 -0700

    attempted merge nodes between zeros without looking up solution

---
+++ MediumInterviewQuestions/LinkedList/MergeNodesBetweenZeros.js
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/merge-nodes-in-between-zeros/
+
+var mergeNodes = function (head) {
+  let newList = new ListNode(0);
+  let dummy = newList;
+  let addUp = false;
+  let sum = 0;
+  while (head) {
+    if (head.val == 0 && addUp === false) {
+      sum = 0;
+      addUp = true;
+      head = head.next;
+    } else if (head.val == 0 && addUp === true) {
+      newList.next = new ListNode(sum);
+      newList = newList.next;
+      head = head.next;
+      sum = 0;
+    } else {
+      sum = sum + head.val;
+      head = head.next;
+    }
+  }
+  return dummy.next;
+};
