a4ed07e216a3b7259bb83385cede4e85e5df3efb
marynghiem <marynghiem46@gmail.com>
2022-05-20 16:17:01 -0700

    joseph provide a better solution to look through a hash map

--- InterviewQuestions/HashMap/SumOfUniqueElements.js
+++ InterviewQuestions/HashMap/SumOfUniqueElements.js
@@ -5,13 +5,23 @@ var sumOfUnique = function (nums) {
   let sum = 0;
 
   for (let i = 0; i < nums.length; i++) {
-    nums[i] in hash ? (hash[nums[i]] = hash[nums[i]] + 1) : (hash[nums[i]] = 1);
+    //nums[i] in hash ? (hash[nums[i]] = hash[nums[i]] + 1) : (hash[nums[i]] = 1);
+    hash[nums[i]] = nums[i] in hash ? hash[nums[i]] + 1 : 1;
   }
+
+  //Josephs solution instead of a for loop:
+  //   for (const key in hash) {
+  //       if (hash[key] === 1) {
+  //         sum += Number(key);
+  //       }
+  //   }
   for (let i = 0; i < nums.length; i++) {
     if (hash[nums[i]] == 1) {
+      // never use double equals
       sum = sum + nums[i];
       delete hash[nums[i]];
     }
   }
+
   return sum;
 };
