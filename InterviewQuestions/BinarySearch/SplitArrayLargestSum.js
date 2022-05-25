4ad182f19ce2870c301a4724fe54d9b363fb57c7
marynghiem <marynghiem46@gmail.com>
2022-05-25 13:18:24 -0700

    completed split array largest sum

---
+++ InterviewQuestions/BinarySearch/SplitArrayLargestSum.js
@@ -0,0 +1,35 @@
+//https://leetcode.com/problems/split-array-largest-sum/
+
+//similar to capacity to ship problem
+var splitArray = function (nums, m) {
+  const feasible = (threshold) => {
+    let total = 0;
+    let count = 1;
+    for (const num of nums) {
+      total += num;
+      if (total > threshold) {
+        total = num;
+        count++;
+        if (count > m) {
+          return false;
+        }
+      }
+    }
+    return true;
+  };
+
+  let left = Math.max(...nums);
+  let right = nums.reduce(function (a, b) {
+    return a + b;
+  }, 0);
+  console.log(right);
+  while (left < right) {
+    let mid = Math.floor(left + (right - left) / 2);
+    if (feasible(mid)) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left;
+};
