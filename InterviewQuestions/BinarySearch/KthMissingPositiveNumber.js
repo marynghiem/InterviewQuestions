dd82e4e1b3dcdd08e61186f786f18a7cd87f3d90
marynghiem <marynghiem46@gmail.com>
2022-05-31 11:25:36 -0700

    annotated the binary search problem kth missing positive number

--- InterviewQuestions/BinarySearch/KthMissingPositiveNumber.js
+++ InterviewQuestions/BinarySearch/KthMissingPositiveNumber.js
@@ -6,6 +6,7 @@ var findKthPositive = function (arr, k) {
   while (left < right) {
     let mid = Math.floor(left + (right - left) / 2);
     if (arr[mid] - mid - 1 < k) {
+      //subtracting current number with index - 1 will give the first number that is missing and so forth
       left = mid + 1;
     } else {
       right = mid;
@@ -13,3 +14,9 @@ var findKthPositive = function (arr, k) {
   }
   return left + k;
 };
+//[2,3,4,7,11], 5
+//output should be 9
+//mid = 2,4, 3
+//so for the condition,
+//4-2-1 = 1 < 5
+//11-4 - 1 = 5
