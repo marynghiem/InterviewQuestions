e803d48b864c41487725598d96ce65d421ad7592
marynghiem <marynghiem46@gmail.com>
2022-06-27 13:47:51 -0700

    completed rectangle overlap

---
+++ InterviewQuestions/2Dproblems/RectangleOverlap.js
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/rectangle-overlap/submissions/
+
+var isRectangleOverlap = function (rec1, rec2) {
+  if (rec1[0] < rec2[2] && rec1[2] > rec2[0] && rec1[1] < rec2[3] && rec1[3] > rec2[1]) {
+    return true;
+  } else {
+    return false;
+  }
+};
