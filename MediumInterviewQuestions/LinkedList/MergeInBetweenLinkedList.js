18046c9b2a0ecf5c8763468ab614bc8739eb9d6f
marynghiem <marynghiem46@gmail.com>
2022-06-27 15:23:29 -0700

    completed medium linked list problem called mergeInBetweenLinkedList

---
+++ MediumInterviewQuestions/LinkedList/MergeInBetweenLinkedList.js
@@ -0,0 +1,24 @@
+var mergeInBetween = function (list1, a, b, list2) {
+  let current = new ListNode();
+  let dummy = current;
+  let i = 0;
+  while (list1) {
+    if (i == a) {
+      while (list2) {
+        current.next = list2;
+        current = current.next;
+        list2 = list2.next;
+      }
+    }
+    if (i < a || i > b) {
+      current.next = list1;
+      current = current.next;
+      list1 = list1.next;
+      i++;
+    } else {
+      i++;
+      list1 = list1.next;
+    }
+  }
+  return dummy.next;
+};
