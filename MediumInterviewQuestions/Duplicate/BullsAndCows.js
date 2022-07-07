4a2bea5e831a52e100b978ba00044e0f80d2f392
marynghiem <marynghiem46@gmail.com>
2022-07-07 14:15:07 -0700

    added my own solution for bulls and cows

--- MediumInterviewQuestions/Duplicate/BullsAndCows.js
+++ MediumInterviewQuestions/Duplicate/BullsAndCows.js
@@ -1,4 +1,50 @@
-//
+//https://leetcode.com/problems/bulls-and-cows/
+
+//my solution, ask joseph how to do it better
+var getHint = function (secret, guess) {
+  let secretSplit = secret.split("");
+  let guessSplit = guess.split("");
+  let answer = "";
+  let bullCount = 0;
+  let cowCount = 0;
+  let correctIndex = [];
+
+  //count how many bulls
+  for (let i = 0; i < secret.length; i++) {
+    if (secretSplit[i] === guessSplit[i]) {
+      bullCount++;
+      correctIndex.push(i);
+    }
+    //remove bulls from array
+  }
+  for (let i = correctIndex.length - 1; i >= 0; i--) {
+    secretSplit.splice(correctIndex[i], 1);
+    guessSplit.splice(correctIndex[i], 1);
+  }
+  //check for cows
+  let secretPointer = 0;
+  let guessPointer = 0;
+
+  while (secretPointer < secretSplit.length) {
+    console.log(1);
+    if (secretSplit[secretPointer] === guessSplit[guessPointer]) {
+      cowCount++;
+      secretSplit.splice(secretPointer, 1);
+      guessSplit.splice(guessPointer, 1);
+      secretPointer = 0;
+      guessPointer = 0;
+    } else {
+      guessPointer++;
+      if (guessPointer > guessSplit.length) {
+        guessPointer = 0;
+        secretPointer++;
+      }
+    }
+  }
+
+  answer = bullCount + "A" + cowCount + "B";
+  return answer;
+};
 
 //correct answer
 var getHint = function (secret, guess) {
