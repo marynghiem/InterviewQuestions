ef4c766d5c3e5015ca515a757510476600b883b2
marynghiem <marynghiem46@gmail.com>
2022-07-08 03:27:12 -0700

    fixed the solution above for bulls and cows problem

--- MediumInterviewQuestions/Duplicate/BullsAndCows.js
+++ MediumInterviewQuestions/Duplicate/BullsAndCows.js
@@ -46,6 +46,52 @@ var getHint = function (secret, guess) {
   return answer;
 };
 
+//fixed the solution above
+var getHint = function (secret, guess) {
+  let secretSplit = secret.split("");
+  let guessSplit = guess.split("");
+  let answer = "";
+  let bullCount = 0;
+  let cowCount = 0;
+  let correctIndex = [];
+
+  //count how many bulls
+  for (let i = 0; i < secret.length; i++) {
+    if (secretSplit[i] === guessSplit[i]) {
+      bullCount++;
+      correctIndex.push(i);
+    }
+    //remove bulls from array
+  }
+  for (let i = correctIndex.length - 1; i >= 0; i--) {
+    secretSplit.splice(correctIndex[i], 1);
+    guessSplit.splice(correctIndex[i], 1);
+  }
+  //check for cows
+  let secretHash = {};
+  let guessHash = {};
+  for (let i = 0; i < secretSplit.length; i++) {
+    secretHash[secretSplit[i]] = secretSplit[i] in secretHash ? secretHash[secretSplit[i]] + 1 : 1;
+    guessHash[guessSplit[i]] = guessSplit[i] in guessHash ? guessHash[guessSplit[i]] + 1 : 1;
+  }
+  console.log(secretHash);
+  console.log(guessHash);
+  //make the set unique
+  const unique = secretSplit.filter((x, i, a) => a.indexOf(x) == i);
+
+  for (let i = 0; i < unique.length; i++) {
+    if (unique[i] in secretHash && unique[i] in guessHash) {
+      firstNum = parseInt(secretHash[unique[i]]);
+      secondNum = parseInt(guessHash[unique[i]]);
+      console.log(Math.min(firstNum, secondNum));
+      cowCount = cowCount + Math.min(firstNum, secondNum);
+    }
+  }
+
+  answer = bullCount + "A" + cowCount + "B";
+  return answer;
+};
+
 //correct answer
 var getHint = function (secret, guess) {
   var map = {};
