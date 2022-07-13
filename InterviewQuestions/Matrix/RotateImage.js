2f3e9be62aff83d851c16896a5bd91638e2496cf
marynghiem <marynghiem46@gmail.com>
2022-07-13 13:20:35 -0700

    added explanation for rotate image

--- InterviewQuestions/Matrix/RotateImage.js
+++ InterviewQuestions/Matrix/RotateImage.js
@@ -5,3 +5,27 @@ var rotate = function (matrix) {
   for (let i in matrix) for (let j = 0; j < i; j++) [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
   return matrix;
 };
+
+//Explantion
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+][
+  //First we reverse the matrix
+
+  ([7, 8, 9], [4, 5, 6], [1, 2, 3])
+][
+  //And then we transpose the matrix, please notice the nested loop, basically we exchange these two parts:
+  //Part1
+
+  ([, ,], [4, ,], [1, 2])
+][
+  //Part2
+
+  ([, 8, 9], [, , 6], [, ,])
+][
+  //And here you go
+
+  [7, 4, 1][(8, 5, 2)][(9, 6, 3)]
+];
