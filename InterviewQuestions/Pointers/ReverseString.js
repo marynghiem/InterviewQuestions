cb111068d03e02c8240d4b54f922d3adcd82a92a
marynghiem <marynghiem46@gmail.com>
2022-10-05 16:10:18 -0700

    completed reverse a string without using extra memory

--- InterviewQuestions/Pointers/ReverseString.js
+++ InterviewQuestions/Pointers/ReverseString.js
@@ -12,3 +12,19 @@ var reverseString = function (s) {
   }
   return s;
 };
+
+//attempted second time
+//can do without help but glanced at the answer once
+var reverseString = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+  let temp;
+  while (left < right) {
+    temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+    left++;
+    right--;
+  }
+  return s;
+};
