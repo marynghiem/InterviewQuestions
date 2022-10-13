e5800687e510f5daecbba41f927224caa4c0b39f
marynghiem <marynghiem46@gmail.com>
2022-10-13 16:01:38 -0700

    completed isHappy problem but made small mistakes with condition and how I wrote the sets

--- InterviewQuestions/Sets/HappyNumber.js
+++ InterviewQuestions/Sets/HappyNumber.js
@@ -18,3 +18,24 @@ var isHappy = function (n) {
   }
   return true;
 };
+
+//attempted a second time and made a mistake with typing out Set and wrote the wrong condition
+
+var isHappy = function (n) {
+  let newNum = 0;
+  let currentNum = n;
+  let previousNum = new Set();
+  while (currentNum !== 1) {
+    if (previousNum.has(currentNum)) {
+      return false;
+    }
+    previousNum.add(currentNum);
+    while (currentNum > 0) {
+      newNum = newNum + (currentNum % 10) ** 2;
+      currentNum = Math.floor(currentNum / 10);
+    }
+    currentNum = newNum;
+    newNum = 0;
+  }
+  return true;
+};
