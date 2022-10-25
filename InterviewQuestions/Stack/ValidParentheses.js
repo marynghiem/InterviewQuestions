20294bce78ec7812011ad2908a72db5070ac80d5
marynghiem <marynghiem46@gmail.com>
2022-10-25 13:38:56 -0700

    completed valid parentheses

--- InterviewQuestions/Stack/ValidParentheses.js
+++ InterviewQuestions/Stack/ValidParentheses.js
@@ -59,3 +59,22 @@ var isValid = function (s) {
   }
   return stack.length ? false : true;
 };
+
+//attempted again. forgot about condition if it started with a "]". so i need a false condition
+var isValid = function (s) {
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
+      stack.push(s[i]);
+    } else if (
+      (s[i] === ")" && stack[stack.length - 1] === "(") ||
+      (s[i] === "]" && stack[stack.length - 1] === "[") ||
+      (s[i] === "}" && stack[stack.length - 1] === "{")
+    ) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+  return stack.length ? false : true;
+};
