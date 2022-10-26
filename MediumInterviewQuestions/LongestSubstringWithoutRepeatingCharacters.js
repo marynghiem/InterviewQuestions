1e4e2db4f68f346c0f11f70ed99b2d6a72aeecb7
marynghiem <marynghiem46@gmail.com>
2022-10-26 14:14:15 -0700

    completed longest substring without repeating characters with sliding window

--- MediumInterviewQuestions/LongestSubstringWithoutRepeatingCharacters.js
+++ MediumInterviewQuestions/LongestSubstringWithoutRepeatingCharacters.js
@@ -42,3 +42,19 @@ var lengthOfLongestSubstring = function (s) {
   }
   return highestSubstring;
 };
+
+//solution to longest substring
+function lengthOfLongestSubstring(s) {
+  let seen = new Set();
+  let longest = 0;
+  let l = 0;
+  for (let r = 0; r < s.length; r++) {
+    while (seen.has(s[r])) {
+      seen.delete(s[l]);
+      l++;
+    }
+    seen.add(s[r]);
+    longest = Math.max(longest, seen.size);
+  }
+  return longest;
+}
