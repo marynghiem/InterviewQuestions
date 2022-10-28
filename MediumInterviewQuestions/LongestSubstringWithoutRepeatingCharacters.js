006121043cef4910f0470a08c07b5f1f5d4fade3
marynghiem <marynghiem46@gmail.com>
2022-10-28 14:06:49 -0700

    completed longest substring with sliding window

--- MediumInterviewQuestions/LongestSubstringWithoutRepeatingCharacters.js
+++ MediumInterviewQuestions/LongestSubstringWithoutRepeatingCharacters.js
@@ -83,3 +83,19 @@ Set(0) {}
 Set(1) { 'b' }
 
 */
+
+//attempt at sliding window
+const lengthOfLongestSubstring = (s) => {
+  let left = 0;
+  let longest = 0;
+  let seen = new Set();
+  for (let right = 0; right < s.length; right++) {
+    while (seen.has(s[right])) {
+      seen.delete(s[left]);
+      left++;
+    }
+    seen.add(s[right]);
+    longest = Math.max(longest, seen.size);
+  }
+  return longest;
+};
