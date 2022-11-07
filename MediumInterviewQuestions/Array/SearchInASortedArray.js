052db3144b5049f789c9ba7728b4163e8e3ae774
marynghiem <marynghiem46@gmail.com>
2022-11-07 11:51:58 -0800

    attempted search a sorted array again with binary search, made minor mistakes

--- MediumInterviewQuestions/Array/SearchInASortedArray.js
+++ MediumInterviewQuestions/Array/SearchInASortedArray.js
@@ -34,3 +34,31 @@ var search = function (nums, target) {
   }
   return -1;
 };
+
+//attempted it again
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  let middle; //initialized it inside the
+  while (left <= right) {
+    middle = Math.floor((left + right) / 2); //put nums.length instead of left + right && forgot to put (left +right) in parentheses
+    if (nums[middle] === target) {
+      return middle;
+    }
+    if (nums[left] <= nums[middle]) {
+      if (nums[left] <= target && target <= nums[middle]) {
+        right = middle - 1;
+      } else {
+        left = middle + 1; //forgot the else statements
+      }
+    } else {
+      if (nums[middle] <= target && target <= nums[right]) {
+        //didnt add the && statement, accidentally wrote it as nums[middle] <=target<= nums[right]
+        left = middle + 1; //accidentally put left = left + 1;
+      } else {
+        right = middle - 1; //forgot the else statements
+      }
+    }
+  }
+  return -1;
+};
