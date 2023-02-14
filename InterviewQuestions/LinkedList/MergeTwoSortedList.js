ae51cef275a9c9003d99ea706f1ab67fcde25a1d
marynghiem <marynghiem46@gmail.com>
2023-02-14 14:44:03 -0800

    finished merge two sorted list but needs more practice because i forgot to increment current

--- InterviewQuestions/LinkedList/MergeTwoSortedList.js
+++ InterviewQuestions/LinkedList/MergeTwoSortedList.js
@@ -123,3 +123,26 @@ var mergeTwoLists = function (list1, list2) {
   }
   return result;
 };
+
+//attempted
+var mergeTwoLists = function (list1, list2) {
+  let current = new ListNode();
+  let dummy = current;
+  while (list1 && list2) {
+    if (list1.val <= list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+  if (list1) {
+    current.next = list1;
+  }
+  if (list2) {
+    current.next = list2;
+  }
+  return dummy.next;
+};
