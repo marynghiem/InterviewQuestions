0ec5c8a1a335976319f29b863316b61aaf7b2d8b
marynghiem <marynghiem46@gmail.com>
2023-03-27 10:00:32 -0700

    use recursion to solve power problem

--- MediumInterviewQuestions/Power.js
+++ MediumInterviewQuestions/Power.js
@@ -1,3 +1,4 @@
+//failed because it exceeded the time limit
 var myPow = function (x, n) {
   let answer = 1;
   if (n >= 1) {
@@ -14,4 +15,26 @@ var myPow = function (x, n) {
     return 1 / answer;
   }
   return answer;
+};
+
+//simplified but still time consuming
+var myPow = function (x, n) {
+  let answer = 1;
+  let positiveN = Math.abs(n);
+  for (let i = 0; i < positiveN; i++) {
+    answer = answer * x;
+  }
+  return n >= 0 ? answer : 1 / answer;
+};
+
+//recursion to solve problem
+
+var myPow = function (x, n) {
+  if (n === 0) return 1;
+
+  let pow = Math.abs(n);
+
+  let result = pow % 2 === 0 ? myPow(x * x, pow / 2) : myPow(x * x, (pow - 1) / 2) * x;
+
+  return n < 0 ? 1 / result : result;
 };
