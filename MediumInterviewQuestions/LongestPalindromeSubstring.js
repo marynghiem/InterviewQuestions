02ebbf943c23d72e127dd72da5dd95df359f74cf
marynghiem <marynghiem46@gmail.com>
2023-03-27 10:51:07 -0700

    found correct solution but need to study about why there is two left and right

--- MediumInterviewQuestions/LongestPalindromeSubstring.js
+++ MediumInterviewQuestions/LongestPalindromeSubstring.js
@@ -1,3 +1,5 @@
+//incorrect answer because time limit was exceeded
+
 var longestPalindrome = function (s) {
   let highestCount = 0;
   let hash = {};
@@ -31,3 +33,35 @@ var isPalindrome = function (s) {
     return false;
   }
 };
+
+//correct solution
+
+var longestPalindrome = function (s) {
+  //condition that the string has to be longer than 2 letters
+  if (s.length < 2) return s;
+  //max is the final answer
+  let max = "";
+  for (let i = 0; i < s.length; i++) {
+    //created a helper function
+    let left = helper(i, i, s);
+    let right = helper(i, i + 1, s);
+    //this is the current max for this loop
+    let currMax = left.length >= right.length ? left : right;
+    //but if the current max is higher than the biggest max, replace it
+    max = currMax.length > max.length ? currMax : max;
+  }
+
+  return max;
+};
+
+function helper(left, right, s) {
+  let curr = "";
+  //given the left, right and s, checking if the numbers are within limits and if it is a palidrome
+  while (left >= 0 && right < s.length && s[left] === s[right]) {
+    curr = s.substring(left, right + 1);
+    left -= 1;
+    right += 1;
+  }
+  //returning the string that is a palidrome
+  return curr;
+}
