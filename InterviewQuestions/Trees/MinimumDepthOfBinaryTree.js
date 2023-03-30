54b67b661439506022faa9a74da833c691752a5a
marynghiem <marynghiem46@gmail.com>
2023-03-30 15:57:06 -0700

    attempted min depth again and got it coorect

--- InterviewQuestions/Trees/MinimumDepthOfBinaryTree.js
+++ InterviewQuestions/Trees/MinimumDepthOfBinaryTree.js
@@ -3,11 +3,32 @@
 var minDepth = function (root) {
   if (root === null) return 0;
 
+  //found the length for left
   const left = minDepth(root.left) + 1;
+  //found teh length for right
   const right = minDepth(root.right) + 1;
 
+  //at the end, if there is no left to begin with, then return right and vice versa
   if (!root.left) return right;
   if (!root.right) return left;
 
+  //return the minimum between the two
+  return Math.min(left, right);
+};
+
+//attempt 1
+var minDepth = function (root) {
+  if (root === null) {
+    return 0;
+  }
+  let left = minDepth(root.left) + 1;
+  let right = minDepth(root.right) + 1;
+
+  if (!root.left) {
+    return right;
+  }
+  if (!root.right) {
+    return left;
+  }
   return Math.min(left, right);
 };
