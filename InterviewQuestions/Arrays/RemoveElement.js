48b5fa0660f45f0ab63f87cfc0f5e7306e5244c6
marynghiem <marynghiem46@gmail.com>
2023-03-30 14:00:54 -0700

    completed remove element

---
+++ InterviewQuestions/Arrays/RemoveElement.js
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/remove-element/description/
+var removeElement = function (nums, val) {
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== val) {
+      nums[i] = nums[j];
+      i++;
+    }
+  }
+  return i;
+};
