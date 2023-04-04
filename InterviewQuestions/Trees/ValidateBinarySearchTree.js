089b2071f04a0874c70f99fe79547c6b61b319fc
marynghiem <marynghiem46@gmail.com>
2023-04-04 11:38:43 -0700

    correct answer in validate binary search tree

--- InterviewQuestions/Trees/ValidateBinarySearchTree.js
+++ InterviewQuestions/Trees/ValidateBinarySearchTree.js
@@ -28,3 +28,15 @@ const rightCheckFunc = (root, right) => {
   }
   return rightCheckFunc(root.right, root.right.right);
 };
+
+//correct solution
+var isValidBST = function (root) {
+  if (!root) return true;
+  return dfs(root, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
+
+  function dfs(root, min, max) {
+    if (!root) return true;
+    if (root.val <= min || root.val >= max) return false;
+    return dfs(root.left, min, root.val) && dfs(root.right, root.val, max);
+  }
+};
