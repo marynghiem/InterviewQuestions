df2deb83586af5a6ed5c30cd758b2a93ba872758
marynghiem <marynghiem46@gmail.com>
2023-04-17 13:08:06 -0700

    completed count digits.js

---
+++ InterviewQuestions/Number/CountDigits.js
@@ -0,0 +1,37 @@
+/*
+Given an integer num, return the number of digits in num that divide num.
+
+An integer val divides nums if nums % val == 0.
+
+ 
+
+Example 1:
+
+Input: num = 7
+Output: 1
+Explanation: 7 divides itself, hence the answer is 1.
+Example 2:
+
+Input: num = 121
+Output: 2
+Explanation: 121 is divisible by 1, but not 2. Since 1 occurs twice as a digit, we return 2.
+Example 3:
+
+Input: num = 1248
+Output: 4
+Explanation: 1248 is divisible by all of its digits, hence the answer is 4.
+ 
+*/
+
+var countDigits = function (num) {
+  let newNum = num;
+  let count = 0;
+  while (newNum > 0) {
+    let lastNum = newNum % 10;
+    newNum = Math.floor(newNum / 10);
+    if (num % lastNum === 0) {
+      count++;
+    }
+  }
+  return count;
+};
