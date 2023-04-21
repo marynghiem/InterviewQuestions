076bb7670f77f151b9cc0a1ed9ede5e06782d4d5
marynghiem <marynghiem46@gmail.com>
2023-04-21 13:14:14 -0700

    annotated reverse string

--- InterviewQuestions/Pointers/ReverseString.js
+++ InterviewQuestions/Pointers/ReverseString.js
@@ -28,3 +28,17 @@ var reverseString = function (s) {
   }
   return s;
 };
+
+const reverseString = (s) => {
+  let left = 0;
+  let right = s.length - 1;
+  let temp;
+  while (left < right) {
+    temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+    left++;
+    right--;
+  }
+  return s;
+};
