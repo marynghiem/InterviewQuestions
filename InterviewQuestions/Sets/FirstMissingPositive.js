8a50104730acdf4c79eac80ea607e71c9cd7539f
marynghiem <marynghiem46@gmail.com>
2023-04-26 13:08:41 -0700

    annotated and completed first missing positive

--- InterviewQuestions/Sets/FirstMissingPositive.js
+++ InterviewQuestions/Sets/FirstMissingPositive.js
@@ -14,3 +14,24 @@ var firstMissingPositive = function (nums) {
     }
   }
 };
+
+//second attempt
+var firstMissingPositive = function (nums) {
+  //create set
+  let set = new Set();
+  //loop through the nums and add every number
+  //so memory is o(n)
+  for (let i = 0; i < nums.length; i++) {
+    set.add(nums[i]);
+  }
+  //look through each number
+  let positiveCheck = 1;
+  while (true) {
+    if (set.has(positiveCheck)) {
+      //if the set contains the number, then increment
+      positiveCheck++;
+    } else {
+      return positiveCheck;
+    }
+  }
+};
