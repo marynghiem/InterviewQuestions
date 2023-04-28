e0eb5a84420681dc240be9cf5ee0d925c46c87d1
marynghiem <marynghiem46@gmail.com>
2023-04-28 13:24:18 -0700

    completed second attempt at group anagrams

--- InterviewQuestions/HashMap/GroupAnagrams.js
+++ InterviewQuestions/HashMap/GroupAnagrams.js
@@ -19,3 +19,26 @@ var groupAnagrams = function (strs) {
   return values;
   //returns [['eat','tea'],['tan','nat']]
 };
+
+//2nd attempt
+
+const groupAnagrams = (strs) => {
+  //created a hash map
+  let hash = {};
+  //loop through the words
+  for (let i = 0; i < strs.length; i++) {
+    //then sort the letters of the word and split as well
+    let letters = strs[i].split("").sort();
+    //check in hash map if the split up words are already in there, if it is, then i will push another word into the array.
+    //else create a new key value pair with the key being the letters and the word being the value
+    hash[letters] ? hash[letters].push(strs[i]) : (hash[letters] = [strs[i]]);
+  }
+  //get all the keys from the hash
+  const keys = Object.keys(hash);
+  //get all the values and make it into an array
+  const values = keys.map((v) => {
+    return hash[v];
+  });
+  //return answer
+  return values;
+};
