958bbcda667a181487ca69103aacd30e115a2a00
marynghiem <marynghiem46@gmail.com>
2023-05-01 12:54:31 -0700

    completed valid parentheses but made very little many mistakes

--- InterviewQuestions/Stack/ValidParentheses.js
+++ InterviewQuestions/Stack/ValidParentheses.js
@@ -98,3 +98,22 @@ const isValid = (s) => {
   }
   return stack.length ? false : true;
 };
+//attempted again and but made a mistake with false and true at the end
+
+var isValid = function (s) {
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+      stack.push(s[i]);
+    } else if (
+      (s[i] === ")" && stack[stack.length - 1] === "(") ||
+      (s[i] === "]" && stack[stack.length - 1] === "[") ||
+      (s[i] === "}" && stack[stack.length - 1] === "{")
+    ) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+  return stack.length ? false : true;
+};
