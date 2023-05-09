ee215d3e2f8bd72bb0e5e83b5cb535e25e2afc53
marynghiem <marynghiem46@gmail.com>
2023-05-09 13:19:12 -0700

    completed length of the last word

--- InterviewQuestions/Strings/LengthOfTheLastWord.js
+++ InterviewQuestions/Strings/LengthOfTheLastWord.js
@@ -9,3 +9,17 @@ var lengthOfLastWord = function (s) {
   });
   return words[words.length - 1].length; //returns the length
 };
+
+//second attempt. couldnt write the regex but could understand
+var lengthOfLastWord = function (s) {
+  //split the words up
+  let words = s.split(" ");
+  console.log(words);
+  //filtering out non words
+  words = words.filter(function (str) {
+    return /\S/.test(str);
+  });
+  console.log(words);
+  //returning the last words characters
+  return words[words.length - 1].length;
+};
