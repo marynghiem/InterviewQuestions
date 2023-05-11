4741e0f7bbf52e18ef10dc0d12fe3e0e4fbaaf25
marynghiem <marynghiem46@gmail.com>
2023-05-11 12:52:53 -0700

    Completed 90% on own but understands the problem of longest substring without repeating characters

--- MediumInterviewQuestions/LongestSubstringWithoutRepeatingCharacters.js
+++ MediumInterviewQuestions/LongestSubstringWithoutRepeatingCharacters.js
@@ -121,3 +121,18 @@ var lengthOfLongestSubstring = function (s) {
   }
   return biggest;
 };
+
+var lengthOfLongestSubstring = function (s) {
+  let seen = new Set();
+  let biggest = 0;
+  let left = 0;
+  for (let i = 0; i < s.length; i++) {
+    while (seen.has(s[i])) {
+      seen.delete(s[left]);
+      left++;
+    }
+    seen.add(s[i]);
+    biggest = Math.max(biggest, seen.size);
+  }
+  return biggest;
+};
